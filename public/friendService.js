a4aeb1d6ace7b84fc05a88cbe71b219b6d9a931b
MikeB <burton.mj@gmail.com>
2017-03-18 13:16:22 -0600

    Luke got it working by putting a .then promise into the service that connects both front end controllers and creating a 'this' statement around response that both controllers could then access

--- public/friendService.js
+++ public/friendService.js
@@ -9,21 +9,25 @@ angular.module('userProfiles')
         method: 'POST',
         url: '/api/login',
         data: {name: user.name, password: user.password}
+      }).then((response) => {
+
+        this.currentUser = response.data.currentUser;
+        return response
       })
     };
 
 
-    this.getFriends = function(user) {
-    	return $http({
-        method: 'POST',
-        url: '/api/login',
-        data: {name: user.name, password: user.password}
-      }).then(function(response){
-        console.log('this is response.data', response.data);
-        return response.data
-      })
-    };
-
-    this.currentUser = this.getFriends();
+    // this.getFriends = function(user) {
+    // 	return $http({
+    //     method: 'POST',
+    //     url: '/api/login',
+    //     data: {name: user.name, password: user.password}
+    //   }).then(function(response){
+    //     console.log('this is response.data', response.data);
+    //     return response.data
+    //   })
+    // };
+    //
+    // this.currentUser = this.getFriends();
 
 });
--- public/profileCtrl.js
+++ public/profileCtrl.js
@@ -1,5 +1,13 @@
 angular.module('userProfiles')
 .controller('profileCtrl', function( $scope, friendService, $http ) {
+
+
+	$scope.currentUser = friendService.currentUser;
+	$scope.friends = friendService.currentUser.friends;
+	console.log('xxx', $scope.currentUser)
+
+
+
 	// FIX ME - assign values to $scope.currentUser and $scope.friends
 
   // $scope.currentUser = friendService.currentUser;
